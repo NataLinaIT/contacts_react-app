ce57e737607f92507a5277508164e4f543ba38d1
nch <nch@anadeainc.com>
2021-06-28 20:35:36 +0300

    Add filter by name

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -1,6 +1,7 @@
+import { useState } from "react";
 import { useContacts } from "./useContacts";
 import { makeStyles, createStyles } from "@material-ui/core/styles";
-import { Box, CircularProgress, Typography } from "@material-ui/core";
+import { Box, CircularProgress, Typography, TextField } from "@material-ui/core";
 import { ContactsTable } from "./ContactsTable/ContactsTable";
 import Grid from "@material-ui/core/Grid";
 import Container from "@material-ui/core/Container";
@@ -16,13 +17,41 @@ const useStyles = makeStyles((theme) =>
     headContainer: {
       marginBottom: theme.spacing(3),
     },
+    filtersContainer: {
+      marginBottom: theme.spacing(3),
+    },
   })
 );
+
+const FiltersDefaultValues = {
+  fullname: "",
+};
+
+const filterByFullname = ({ first, last }, fullname) => {
+  return (
+    first?.toLowerCase().includes(fullname.toLowerCase()) ||
+    last?.toLowerCase().includes(fullname.toLowerCase())
+  );
+};
 
 export const Contacts = () => {
   const classes = useStyles();
   const contacts = useContacts();
   const [dataViewMode, setDataViewMode] = useDataVieMode();
+
+  const [filters, setFilters] = useState(FiltersDefaultValues);
+
+  const handleChangeFilter = (event) => {
+    setFilters((prevFilters) => ({
+      ...prevFilters,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  const filteredContacts = contacts.data.filter((c) =>
+    filterByFullname(c.name, filters.fullname)
+  );
+
 
   return (
     <Container className={classes.root}>
@@ -38,6 +67,18 @@ export const Contacts = () => {
             />
           </Box>
         </Grid>
+        <Grid item xs={12} className={classes.filtersContainer}>
+          <Box display="flex">
+            <TextField
+              name="fullname"
+              label="Fullname"
+              variant="outlined"
+              size="small"
+              value={filters.fullname}
+              onChange={handleChangeFilter}
+            />
+          </Box>
+        </Grid>
         <Grid item xs={12}>
           {(() => {
             if (contacts.isLoading) {
@@ -47,7 +88,7 @@ export const Contacts = () => {
               return <div data-testid="contacts-error">... error</div>;
             }
             if (dataViewMode === DATA_VIEW_MODES.TABLE) {
-              return <ContactsTable data={contacts.data} />;
+              return <ContactsTable data={filteredContacts} />;
             }
             if (dataViewMode === DATA_VIEW_MODES.GRID) {
               return <div data-testid="contacts-grid-container">grid</div>;
